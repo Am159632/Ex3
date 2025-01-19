22ceaee54a145f137a9eef460f7f850c5dd4088d
Am159632 <asaf.margaliot@gmail.com>
2025-01-20 00:19:26 +0200

    1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16

--- src/Main.java
+++ src/Main.java
@@ -271,6 +271,74 @@ public class Main {
 
         return i + 1;
     }
+    //14
+    public class Point2D {
+        public static final double EPS = 0.001;
+        public final Point2D ORIGIN =new Point2D(0,0);
+        private double _x, _y;
+        public Point2D(double a,double b) {_x=a; _y=b;  }  // Standard Constructor.
+        public Point2D(Point2D p) { this(p.x(), p.y()); }
+// Copy Constructor
+        /** String Constructor: following this String structure:  "-1.2,5.3" --> (-1.2,5.3) ; */
+        public Point2D(String s) {
+            String[] a = s.split(",");
+            _x = Double.parseDouble(a[0]);
+            _y = Double.parseDouble(a[1]);  }
+        public double x() {return _x;}
+        public double y() {return _y;}
+        public Point2D add(Point2D p) {
+            return new Point2D(p.x()+x(),p.y()+this.y());  }
+        /** Translates this point by a vector like representation of p. */
+        public void move(Point2D p) {_x += p.x(); _y += p.y();}
+        public String toString() {return _x+","+_y; }
+        public double distance() {return this.distance(ORIGIN); }
+        /** distance(this,p2) = Math.sqrt(dx^2 + dy^2) */
+        public double distance(Point2D p2) {
+            double dx = this.x() - p2.x(), dy = this.y() - p2.y();
+            return Math.sqrt(dx*dx+dy*dy);   }
+        /**return true iff: this point equals to p.  */
+        public boolean equals(Object p)  {
+            if(p==null || !(p instanceof Point2D)) {return false;}
+            Point2D p2 = (Point2D)p;
+            return (_x==p2._x) && (_y==p2.y());
+        }
+        public boolean equals(Point2D p) {
+            if(p==null) {return false;}
+            return ((_x==p._x) && (_y==p._y));  }
+        public boolean close2equals(Point2D p2, double eps) {
+            return (this.distance(p2) < eps);
+        }
+    }
+    public class Elipse {
+        private Point2D p;
+        private Point2D q;
+        private double n;
+
+        public Elipse(double Xp, double Yp, double Xq, double Yq, double n) {
+            this.p = new Point2D(Xp, Yp);
+            this.q = new Point2D(Xq, Yq);
+            this.n = n;
+        }
+
+        public Elipse(Elipse e) {
+            this.p = new Point2D(e.p);
+            this.q = new Point2D(e.q);
+            this.n = e.n;
+        }
+
+        public int where(Point2D r) {
+            double distance = this.p.distance(r) + this.q.distance(r);
+            if (distance < this.n)
+                return -1;
+            if (distance == this.n)
+                return 0;
+            return 1;
+        }
+
+        public boolean equals(Elipse e) {
+            return this.p.equals(e.p) && this.q.equals(e.q) && this.n == e.n;
+        }
+    }
     //15,16
     interface Parabula {
         double f(double x); // computes the value of this parabula at x.
